38bb26c119aa22a8205367556de0e917f44f0135
jerry <jerry.liao@spaceadvisor.com>
2018-06-27 10:37:50 +0800

    First commit

---
+++ SpaceConsole/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SpaceConsole
+//
+//  Created by Jerry on 2018/6/27.
+//  Copyright © 2018年 SpaceAdvisor. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SpaceConsole/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SpaceConsole
+//
+//  Created by Jerry on 2018/6/27.
+//  Copyright © 2018年 SpaceAdvisor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
